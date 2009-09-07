d8bf75f4345f4a25cd501d79db0395825cb8fde3
tsaikd <tsaikd@gmail.com>
2009-09-07 22:35:31 +0800

    update lang text

--- lang/ExchangeAlert_zh_TW.ts
+++ lang/ExchangeAlert_zh_TW.ts
@@ -2,58 +2,88 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="zh_TW">
 <context>
+    <name>QWinConfMainApp</name>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="17"/>
+        <source>Hide window when start</source>
+        <translation>啟動程式時自動隱藏視窗</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="24"/>
+        <source>Only update from internet in trading time</source>
+        <translation>只在交易時間內從網路更新資料</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="33"/>
+        <source>Refresh time(sec):</source>
+        <translation>更新間隔(秒):</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="50"/>
+        <source>&amp;Apply</source>
+        <translation>套用(&amp;A)</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="57"/>
+        <source>&amp;Close</source>
+        <translation>關閉(&amp;C)</translation>
+    </message>
+</context>
+<context>
     <name>QWinMainApp</name>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="32"/>
-        <location filename="../src/WinMainApp.cpp" line="135"/>
+        <location filename="../src/WinMainApp.cpp" line="34"/>
+        <location filename="../src/WinMainApp.cpp" line="112"/>
         <source>&amp;Quit</source>
         <translation>離開(&amp;Q)</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="42"/>
         <source>Hide window when start</source>
-        <translation>啟動程式時自動隱藏視窗</translation>
+        <translation type="obsolete">啟動程式時自動隱藏視窗</translation>
     </message>
     <message>
         <source>Only work on trading time</source>
         <translation type="obsolete">只在交易時間內從網路更新資料</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="49"/>
         <source>Only update from internet in trading time</source>
-        <translation>只在交易時間內從網路更新資料</translation>
+        <translation type="obsolete">只在交易時間內從網路更新資料</translation>
     </message>
     <message>
         <source>Refresh time:</source>
         <translation type="obsolete">更新時間:</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="58"/>
         <source>Refresh time(sec):</source>
-        <translation>更新間隔(秒):</translation>
+        <translation type="obsolete">更新間隔(秒):</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="75"/>
+        <location filename="../src/WinMainApp.cpp" line="46"/>
         <source>US dollar:</source>
         <translation>美金:</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="128"/>
+        <location filename="../src/WinMainApp.cpp" line="99"/>
         <source>&amp;Apply</source>
         <translation>套用(&amp;A)</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="221"/>
+        <location filename="../src/WinMainApp.cpp" line="106"/>
+        <source>Extra &amp;Config</source>
+        <translation>額外設定(&amp;C)</translation>
+    </message>
+    <message>
+        <location filename="../src/WinMainApp.cpp" line="193"/>
         <source>US Dollar reached %1</source>
         <translation>美金已達目標 %1</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="319"/>
+        <location filename="../src/WinMainApp.cpp" line="300"/>
         <source>Updating US dollar ...</source>
         <translation>美金更新中...</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="371"/>
+        <location filename="../src/WinMainApp.cpp" line="352"/>
         <source>Updated US dollar</source>
         <translation>美金更新完畢</translation>
     </message>
