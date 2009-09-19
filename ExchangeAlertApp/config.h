637c3d4873ee718638fa8e9cf510153f15c05e9e
tsaikd <tsaikd@gmail.com>
2009-09-19 15:09:16 +0800

    v1.0.0.9

--- ExchangeAlertApp/config.h
+++ ExchangeAlertApp/config.h
@@ -2,8 +2,8 @@
 #define _EXCHANGEALERT_CONFIG_H
 
 #define PROJNAME	"ExchangeAlert"
-#define PROJVER		"1.0.0.8"
-#define PROJVERC	1,0,0,8
+#define PROJVER		"1.0.0.9"
+#define PROJVERC	1,0,0,9
 
 #define ICONPATH	"../icon/ExchangeAlert2.ico"
 
