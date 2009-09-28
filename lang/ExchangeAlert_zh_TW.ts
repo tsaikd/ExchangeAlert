bda574d84d13e6f519d5f30fcd92ffa95e4843d9
tsaikd <tsaikd@gmail.com>
2009-09-28 22:25:05 +0800

    update lang

--- lang/ExchangeAlert_zh_TW.ts
+++ lang/ExchangeAlert_zh_TW.ts
@@ -9,32 +9,52 @@
         <translation>啟動程式時自動隱藏視窗</translation>
     </message>
     <message>
-        <location filename="../src/WinConfMainApp.cpp" line="24"/>
+        <location filename="../src/WinConfMainApp.cpp" line="30"/>
+        <source>Default style</source>
+        <translation>預設樣式</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="31"/>
+        <source>Horizontal style</source>
+        <translation>橫條樣式</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="32"/>
+        <source>Vertical style</source>
+        <translation>垂直樣式</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="41"/>
         <source>Hide status bar</source>
         <translation>隱藏狀態列</translation>
     </message>
     <message>
-        <location filename="../src/WinConfMainApp.cpp" line="33"/>
+        <location filename="../src/WinConfMainApp.cpp" line="50"/>
         <source>Dollar font size:</source>
         <translation>匯率字型大小:</translation>
     </message>
     <message>
-        <location filename="../src/WinConfMainApp.cpp" line="48"/>
+        <location filename="../src/WinConfMainApp.cpp" line="65"/>
         <source>Only update from internet in trading time</source>
         <translation>只在交易時間內從網路更新資料</translation>
     </message>
     <message>
-        <location filename="../src/WinConfMainApp.cpp" line="57"/>
+        <location filename="../src/WinConfMainApp.cpp" line="74"/>
         <source>Refresh time(sec):</source>
         <translation>更新間隔(秒):</translation>
     </message>
     <message>
-        <location filename="../src/WinConfMainApp.cpp" line="74"/>
+        <location filename="../src/WinConfMainApp.cpp" line="89"/>
+        <source>(*): Need to restart this program</source>
+        <translation>(*): 需要重新啟動此程式</translation>
+    </message>
+    <message>
+        <location filename="../src/WinConfMainApp.cpp" line="95"/>
         <source>&amp;Apply</source>
         <translation>套用(&amp;A)</translation>
     </message>
     <message>
-        <location filename="../src/WinConfMainApp.cpp" line="81"/>
+        <location filename="../src/WinConfMainApp.cpp" line="102"/>
         <source>&amp;Close</source>
         <translation>關閉(&amp;C)</translation>
     </message>
@@ -43,7 +63,7 @@
     <name>QWinMainApp</name>
     <message>
         <location filename="../src/WinMainApp.cpp" line="34"/>
-        <location filename="../src/WinMainApp.cpp" line="108"/>
+        <location filename="../src/WinMainApp.cpp" line="125"/>
         <source>&amp;Quit</source>
         <translation>離開(&amp;Q)</translation>
     </message>
@@ -68,32 +88,34 @@
         <translation type="obsolete">更新間隔(秒):</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="46"/>
+        <location filename="../src/WinMainApp.cpp" line="42"/>
         <source>US dollar:</source>
         <translation>美金:</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="95"/>
+        <location filename="../src/WinMainApp.cpp" line="103"/>
+        <location filename="../src/WinMainApp.cpp" line="117"/>
         <source>&amp;Apply</source>
         <translation>套用(&amp;A)</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="102"/>
+        <location filename="../src/WinMainApp.cpp" line="108"/>
+        <location filename="../src/WinMainApp.cpp" line="121"/>
         <source>Extra &amp;Config</source>
         <translation>額外設定(&amp;C)</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="190"/>
+        <location filename="../src/WinMainApp.cpp" line="291"/>
         <source>US Dollar reached %1</source>
         <translation>美金已達目標 %1</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="318"/>
+        <location filename="../src/WinMainApp.cpp" line="419"/>
         <source>Updating US dollar ...</source>
         <translation>美金更新中...</translation>
     </message>
     <message>
-        <location filename="../src/WinMainApp.cpp" line="370"/>
+        <location filename="../src/WinMainApp.cpp" line="471"/>
         <source>Updated US dollar</source>
         <translation>美金更新完畢</translation>
     </message>
